f0db06f1d51cdac9031ef20f19b7a066a42b8d76
Duy Do <anhduycnttx1@gmail.com>
2023-02-12 18:36:29 +0700

    base

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 
-import { StudiosListViewModule } from './studios/studios.module';
-import { ActorsModule } from './actors/actor.module';
-import { CategoryModule } from './category/category.module';
-
 @Module({
-  imports: [UserModule, StudiosListViewModule, ActorsModule, CategoryModule],
+  imports: [UserModule],
   controllers: [],
   providers: [],
 })
