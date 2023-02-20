bddf0b14ea0415b9397f388851c50dbbf37612b4
Đỗ Duy <anhduycnttx1@gmail.com>
2023-02-20 11:43:22 +0700

    up cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
   app.setGlobalPrefix('/api/posi/v1');
   app.useGlobalPipes(new ValidationPipe());
   // Allow access to static file
